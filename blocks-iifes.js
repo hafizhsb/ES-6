f856b01c6a4b079db78c816a4c26c63995363814
DESKTOP-56D20U8\TEMU <hafiztrizer@gmail.com>
2018-08-13 11:44:35 +0700

    Blocks and IIFEs

---
+++ blocks-iifes.js
@@ -0,0 +1,19 @@
+//Blocks and IIFEs
+
+//ES6
+{
+    const a = 1;
+    let b = 2;
+    var c = 3;
+}
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+
+//ES5
+(function(){
+    var c = 3;
+})();
+
+console.log(c);
